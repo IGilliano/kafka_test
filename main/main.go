4b1211191b315940ee926b4bab4cc8787d6773c5
IGilliano <ivv_12@mail.ru>
2022-12-18 15:14:58 +0300

    - Обновил драйвер PGX - Протестировал соединение с базой данных и функцию PostTaskToDB

--- main/main.go
+++ main/main.go
@@ -1,10 +1,25 @@
 package main
 
 import (
-	"kafka_test/kafka"
+	"fmt"
+	"kafka_test/postgres"
+	"log"
+	"time"
 )
 
 func main() {
-	kafka.NewConsumer("test")
+	taskRepository := postgres.NewTaskRepository()
+
+	//kafka.NewConsumer("test")
+
+	var t postgres.DbTask
+	t.Task = "Test"
+	t.Time = time.Now()
+
+	id, err := taskRepository.PostTaskToDB(t)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(id)
 
 }
--- postgres/postges.go
+++ postgres/postges.go
@@ -1,1 +1,32 @@
 package postgres
+
+import (
+	"database/sql"
+	_ "github.com/jackc/pgx/v5/stdlib"
+	"log"
+	"time"
+)
+
+type DbTask struct {
+	Id   int       `json:"id"`
+	Task string    `json:"task"`
+	Time time.Time `json:"time"`
+}
+
+type TaskRepository struct {
+	db *sql.DB
+}
+
+func NewTaskRepository() TaskRepository {
+	db, err := sql.Open("pgx", "postgres://postgres:456123789@localhost:5432/postgres?sslmode=disable")
+	if err != nil {
+		log.Fatal(err)
+	}
+	return TaskRepository{db: db}
+}
+
+func (tr *TaskRepository) PostTaskToDB(t DbTask) (int, error) {
+	var id int
+	err := tr.db.QueryRow("INSERT INTO tasks (task, time) VALUES ($1, $2) RETURNING id", t.Task, t.Time).Scan(&id)
+	return id, err
+}
